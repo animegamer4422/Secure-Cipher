b9bb227fda29231ae07910d14bd6f418b804f227
animegamer4422 <animegamer4422@gmail.com>
2024-03-16 18:59:04 +0530

    remember theme on site refresh

--- app/components/Header.client.tsx
+++ app/components/Header.client.tsx
@@ -3,17 +3,22 @@ import React, { useState, useEffect } from 'react';
 
 const Header = () => {
   // State to manage if the retro theme is active or not
-  const [isretro, setIsretro] = useState(false);
+  const [isRetro, setIsRetro] = useState(() => {
+    // Get the theme from localStorage or default to false if not found
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'retro';
+  });
 
-  // Update the theme whenever the isretro state changes
+  // Update the theme whenever the isRetro state changes
   useEffect(() => {
-    const theme = isretro ? 'retro' : 'default'; // Assuming 'default' is your default theme
+    const theme = isRetro ? 'retro' : 'default'; // Assuming 'default' is your default theme
     document.documentElement.setAttribute('data-theme', theme);
-  }, [isretro]);
+    localStorage.setItem('theme', theme); // Save the theme preference to localStorage
+  }, [isRetro]);
 
   // Handler to toggle the theme based on the checkbox
-  const handleThemeToggle = (e: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
-    setIsretro(e.target.checked);
+  const handleThemeToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsRetro(e.target.checked);
   };
 
   return (
@@ -21,7 +26,7 @@ const Header = () => {
       <h1 className="text-xl">Secure Cipher</h1>
       <input 
         type="checkbox" 
-        checked={isretro} 
+        checked={isRetro} 
         onChange={handleThemeToggle} 
         className="toggle theme-controller"
       />
