caacef3db69cc9638fdc40874a4c42ca208ca71e
animegamer4422 <animegamer4422@gmail.com>
2024-03-16 14:07:40 +0530

    Deleted Remnants from previous project

--- app/ui/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer p-8 text-base-content border-t-2 border-neutral">
-      <p>© {new Date().getFullYear()} Vahala Designs</p>
-    </footer>
-  );
-}
