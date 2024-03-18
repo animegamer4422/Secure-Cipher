cbebca300f0ef7a1667ee07fc7217ef7d26c6411
animegamer4422 <animegamer4422@gmail.com>
2024-03-19 01:42:54 +0530

    ui improvements for theme toggle by adding theme indicators

--- app/components/Header.client.tsx
+++ app/components/Header.client.tsx
@@ -22,17 +22,11 @@ const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center p-4">
       <h1 className="text-xl">Secure Cipher</h1>
-      <label className="toggle-label">
-      <input
-              id="toggle-theme-checkbox"
-              type="checkbox"
-              checked={theme === "retro"}
-              value="retro"
-              onChange={toggleTheme}
-              className="toggle theme-controller"
-            />
-        <span className="toggle-mark"></span>
-      </label>
+<label className="cursor-pointer grid place-items-center">
+  <input type="checkbox" onChange={toggleTheme} value="retro" checked={theme === "retro"} className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+  <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+  <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+</label>
     </header>
   );
 };
