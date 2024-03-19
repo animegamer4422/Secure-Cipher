ce91d3b959ade3a0671d852a938361fdb421509c
animegamer4422 <animegamer4422@gmail.com>
2024-03-19 09:42:16 +0530

    changed the light mode theme to garden instead of retro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dracula", "retro"],
+    themes: ["dracula", "retro", "garden"],
   },
 };
 export default config;
